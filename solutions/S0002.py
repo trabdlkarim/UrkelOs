4fa76e00b15f053f3005c8a7e8f6fadc750bd7f7
Toure A.Karim <contact@trabdlkarim.com>
2019-10-27 00:52:04 +0300

    add solution for P0002

---
+++ solutions/S0002.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 01:11:25 2019
+
+@author: trabdlkarim
+"""
+
+def fibo(n):
+    if n <2:
+        return n
+    else: 
+        return fibo(n-1) + fibo(n-2)
+    
+def main():
+    total=0
+    for i in range(11):
+        fib = fibo(i)
+        if (fib & 1):
+            total += fib
+    print(total)
+
+if __name__=="__main__":
+   main()
